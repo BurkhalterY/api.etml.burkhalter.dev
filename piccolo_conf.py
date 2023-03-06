f68d0773596d80898f48e7cba8a874886ee7d306
Yannis Burkhalter <yannis@burkhalter.dev>
2023-03-06 18:03:56 +0100

    Move db config in a toml file

--- piccolo_conf.py
+++ piccolo_conf.py
@@ -1,14 +1,10 @@
+import tomllib
 from piccolo.conf.apps import AppRegistry
 from piccolo.engine.postgres import PostgresEngine
 
+with open("database.toml", "rb") as f:
+    config = tomllib.load(f)
+
 APP_REGISTRY = AppRegistry(apps=["app.piccolo_app", "piccolo.apps.user.piccolo_app"])
 
-DB = PostgresEngine(
-    config={
-        "database": "etml_burkhalter_dev",
-        "user": "postgres",
-        "password": "toor",
-        "host": "localhost",
-        "port": 5432,
-    }
-)
+DB = PostgresEngine(config=config["db"])
