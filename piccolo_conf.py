6be3db6f4d2f1182328d0d88584ca9556d0f6817
Yannis Burkhalter <yannis@burkhalter.dev>
2023-03-18 19:30:09 +0100

    Add sqlite support

--- piccolo_conf.py
+++ piccolo_conf.py
@@ -1,10 +1,15 @@
 import tomllib
 from piccolo.conf.apps import AppRegistry
 from piccolo.engine.postgres import PostgresEngine
+from piccolo.engine.sqlite import SQLiteEngine
 
 with open("database.toml", "rb") as f:
     config = tomllib.load(f)
 
 APP_REGISTRY = AppRegistry(apps=["app.piccolo_app", "piccolo.apps.user.piccolo_app"])
 
-DB = PostgresEngine(config=config["db"])
+engine = config["general"]["engine"]
+if engine == "sqlite":
+    DB = SQLiteEngine(path=config["sqlite"]["path"])
+if engine == "postgres":
+    DB = PostgresEngine(config=config["postgres"])
