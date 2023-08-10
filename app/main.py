6549eeba2f2c86615d1c66a288b7c72cbd4b721b
Yannis Burkhalter <yannis@burkhalter.dev>
2023-08-10 21:42:50 +0200

    "Security" fix

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ WEB_PORT = int(os.environ.get("WEB_PORT", 8000))
 IS_PROD = ENVIRONMENT == "prod"
 IS_DEV = ENVIRONMENT == "dev"
 
-graphql_app = GraphQL(schema)
+graphql_app = GraphQL(schema, graphiql=IS_DEV)
 app = Starlette()
 app.add_route("/graphql", graphql_app)
 app.add_websocket_route("/graphql", graphql_app)
@@ -27,18 +27,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-ssl_keyfile = "app/config/key.pem"
-ssl_certfile = "app/config/cert.pem"
-if not (os.path.isfile(ssl_keyfile) and os.path.isfile(ssl_certfile)):
-    ssl_keyfile = ssl_certfile = None
-
 
 def start():
-    uvicorn.run(
-        "app.main:app",
-        host="0.0.0.0",
-        port=WEB_PORT,
-        reload=IS_DEV,
-        ssl_keyfile=ssl_keyfile,
-        ssl_certfile=ssl_certfile,
-    )
+    uvicorn.run("app.main:app", host="0.0.0.0", port=WEB_PORT, reload=IS_DEV)
