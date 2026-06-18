3a5f10165256db0edcbeb1f5a4bfd2e03c5d7e63
Yannis Burkhalter <yannis@burkhalter.dev>
2026-06-18 22:13:21 +0200

    fix(Starlette): Fix related to randomly bumping requirements ;)

--- app/main.py
+++ app/main.py
@@ -3,8 +3,10 @@ import os
 import uvicorn
 from dotenv import load_dotenv
 from starlette.applications import Starlette
+from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import PlainTextResponse
+from starlette.routing import Route, WebSocketRoute
 
 from app.schemas import GraphQL, schema
 
@@ -19,17 +21,22 @@ WEB_PORT = int(os.environ.get("WEB_PORT", 8000))
 IS_PROD = ENVIRONMENT == "prod"
 IS_DEV = ENVIRONMENT == "dev"
 
-graphql_app = GraphQL(schema, graphiql=IS_DEV)
-app = Starlette()
-app.add_route("/status", status)
-app.add_route("/graphql", graphql_app)
-app.add_websocket_route("/graphql", graphql_app)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+graphql_app = GraphQL(schema) #, graphiql=IS_DEV)
+app = Starlette(
+    routes=[
+        Route("/status", status),
+        Route("/graphql", graphql_app),
+        WebSocketRoute("/graphql", graphql_app),
+    ],
+    middleware=[
+        Middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+    ]
 )
 
 
